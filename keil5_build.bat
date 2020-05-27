d6746192f3bcbd0a31afd228b669aefe371802db
Jalon <jalonwong@gmail.com>
2020-05-27 17:08:23 +0800

    更新 bat 脚本

--- keil5_build.bat
+++ keil5_build.bat
@@ -2,20 +2,19 @@
 set UV=%~dp0%UV4\UV4.exe
 
 set binpath=%3
-if "%binpath%"=="" (
-    goto end
-) else (
-    if exist %binpath%".bin" (
-        DEL %binpath%".bin"
-    )
-    
-    if exist %binpath%".axf" (
-        DEL %binpath%".axf"
-    )
+if not defined binpath goto build
+if %binpath%=="" goto build
+
+if exist %binpath%".bin" (
+    DEL %binpath%".bin"
 )
-:end
+
+if exist %binpath%".axf" (
+    DEL %binpath%".axf"
+)
+:build
 
 echo Building...
 "%UV%" -j0 %2 %1 -o build_log.txt
-type %~dp1%build_log.txt
+type "%~dp1%""build_log.txt"
 echo Done.
